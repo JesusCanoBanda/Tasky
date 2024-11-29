4138db9041ec4be7e8e07669c9974831e4c6bd4c
ZwTz <jdangro4@gmail.com>
2024-11-29 11:59:52 -0600

    modelos y bd de grupal

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,14 +9,17 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-    use Notifiable;
 
+    /**
+     * Tabla asociada al modelo.
+     *
+     * @var string
+     */
     protected $table = 'users';
 
     /**
-     * The attributes that are mass assignable.
+     * Atributos asignables en masa.
      *
      * @var array<int, string>
      */
@@ -31,7 +34,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * Atributos que deben estar ocultos en la serialización.
      *
      * @var array<int, string>
      */
@@ -41,21 +44,46 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * Los atributos que deben ser casteados.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 
+    /**
+     * Relación con EspacioPersonal.
+     * Un usuario puede tener múltiples espacios personales.
+     */
     public function espacioPersonal()
     {
         return $this->hasMany(EspacioPersonal::class, 'id_user');
     }
 
+    /**
+     * Relación con MiembrosGrupal.
+     * Un usuario puede ser parte de múltiples espacios grupales como miembro.
+     */
+    public function miembrosGrupal()
+    {
+        return $this->hasMany(MiembrosGrupal::class, 'id_usuario');
+    }
+
+    /**
+     * Relación con EspacioGrupal a través de MiembrosGrupal.
+     * Un usuario puede pertenecer a múltiples espacios grupales.
+     */
+    public function espaciosGrupales()
+    {
+        return $this->hasManyThrough(
+            EspacioGrupal::class,
+            MiembrosGrupal::class,
+            'id_usuario', // Clave foránea en MiembrosGrupal
+            'id',         // Clave foránea en EspacioGrupal
+            'id',         // Clave local en User
+            'id_grupal'   // Clave local en MiembrosGrupal
+        );
+    }
 }
