e6b916a9145af0aea96a1edd75a7bf6b9aa0643b
Sebastian Guevara <yungdiablox@gmail.com>
2024-11-29 12:10:20 -0600

    Modificion de Editar el espacio de trabajo

--- resources/views/espaciopersonal/edit.blade.php
+++ resources/views/espaciopersonal/edit.blade.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/editarespacio.css') }}">
     <title>Editar Espacio</title>
 </head>
 <body>
     <div class="results-table">
         <div class="form-title">Editar Espacio</div>
+        <a href="{{ route('espaciopersonal.index') }}">
+            <img class="close" src="{{ asset('images/close.png') }}" alt="Cerrar">
+        </a>
         <form class="player-form" action="{{ route('espaciopersonal.update', $espacio->id) }}" method="POST">
             @csrf
             @method('PUT') <!-- Cambiar a PUT para realizar la actualización -->
