a977afd53d6546084e2124741c74622e6740f219
ZwTz <jdangro4@gmail.com>
2024-11-30 07:30:46 -0600

    antes del merge

---
+++ app/Models/TareaGrupal.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TareaGrupal extends Model
+{
+    use HasFactory;
+
+    /**
+     * La tabla asociada al modelo.
+     *
+     * @var string
+     */
+    protected $table = 'tarea_grupal';
+
+    /**
+     * Los atributos que son asignables en masa.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'fechainicio',
+        'fechafinal',
+        'descripcion',
+        'estado',
+        'porcentaje',
+        'categoria',
+        'id_espacio',
+        'responsable',
+    ];
+
+    /**
+     * Relación con EspacioGrupal.
+     * Una tarea grupal pertenece a un espacio grupal.
+     */
+    public function espacio()
+    {
+        return $this->belongsTo(EspacioGrupal::class, 'id_espacio');
+    }
+}
