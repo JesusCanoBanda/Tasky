f29725f07d214679542d3550cc57395b16ce48b1
Erick Mata <mataverrick>
2024-12-01 23:41:06 -0600

    heroku2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tasky - Gestión Simplificada</title>
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 </head>
 <body class="font-sans antialiased bg-blue-50 text-gray-800">
     <header class="bg-white shadow-md">
