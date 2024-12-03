6f69546739f534ee222430d8f3209b4d157c6666
Sebastian Guevara <yungdiablox@gmail.com>
2024-12-03 13:07:42 -0600

    Libreria eliminada

--- resources/views/espaciogrupal/index.blade.php
+++ resources/views/espaciogrupal/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <link rel="stylesheet" href="{{ asset('css/table.css') }}">
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    @include('sweetalert::alert')
+    
 
     <div class="layout">
         <div
